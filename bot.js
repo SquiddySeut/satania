c9e25b507f8362f1dc61b2c86ef67adb5d016002
SquiddySeut <40527468+SquiddySeut@users.noreply.github.com>
2018-07-07 19:27:46 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,11 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var cs;
+var sp;
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -10,6 +15,21 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
+    if (message.content==="I'll call my friends"){
+        cs=1;
+    }
+    if (message.content==="Satania-sama, how are you?"&&cs===1){
+        sp=getRandomInt(2);
+        if (sp===0){
+            message.reply("I'm fine...");
+        }
+        if (sp===1){
+            message.reply("you lowlife dare ask about the superior archdemon?");
+        }
+        if (sp===3){
+            message.reply("I've seen better days...");
+        }
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
